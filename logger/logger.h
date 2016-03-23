cf48ddb85b6d30e04b00bfa666ac694bbf6afedb
夏奈 <shuaiji.tsj@alibaba-inc.com>
2016-03-23 20:53:20 +0800

    Initial Commit

---
+++ logger/logger.h
@@ -0,0 +1,19 @@
+//
+//  logger.h
+//  logger
+//
+//  Created by 唐帅佶 on 16/3/23.
+//  Copyright © 2016年 xiaoshuai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for logger.
+FOUNDATION_EXPORT double loggerVersionNumber;
+
+//! Project version string for logger.
+FOUNDATION_EXPORT const unsigned char loggerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <logger/PublicHeader.h>
+
+
